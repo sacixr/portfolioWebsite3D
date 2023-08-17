f4590ff95502e1d8eacaa1a269e0717f1e049efd
Wik <zkac176@live.rhul.ac.uk>
2023-08-17 18:23:15 +0100

    Add work experience section

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,10 +21,10 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import nesc from "./company/nesc.png";
+import cex from "./company/cex.png";
+import rhsu from "./company/rhsu.png";
+import rhul from "./company/rhul.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -52,11 +52,11 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
   carrent,
   jobit,
   tripguide,
+  nesc,
+  cex,
+  rhsu,
+  rhul
 };

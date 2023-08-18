29f42295405462e69a1d95a22252b06af66bef30
Wik <zkac176@live.rhul.ac.uk>
2023-08-18 16:03:51 +0100

    Refactor dead code

--- src/assets/index.js
+++ src/assets/index.js
@@ -8,18 +8,12 @@ import menu from "./menu.svg";
 import close from "./close.svg";
 
 import css from "./tech/css.png";
-import docker from "./tech/docker.png";
-import figma from "./tech/figma.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
 import elephantsql from "./tech/elephantsql.png";
-import mongodb from "./tech/mongodb.png";
-import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
-import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
-import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 import gitlab from "./tech/gitlab.png";
 import githublogo from "./tech/github.png";
@@ -45,18 +39,12 @@ export {
   menu,
   close,
   css,
-  docker,
-  figma,
   git,
   html,
   javascript,
   elephantsql,
-  mongodb,
-  nodejs,
   reactjs,
-  redux,
   tailwind,
-  typescript,
   threejs,
   carrent,
   jobit,

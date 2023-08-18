5ae55c1fee245cd403bc41172633393a982ad476
Wik <zkac176@live.rhul.ac.uk>
2023-08-18 16:43:49 +0100

    Add work experience title

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Tilt } from 'react-tilt';
+import { motion } from 'framer-motion';
+import { styles } from '../styles';
+import { github } from '../assets';
+import { projects } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
+import { SectionWrapper } from '../hoc';
 
 const Works = () => {
   return (
-    <div>Works</div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Some Things I've Worked On</p>
+        <h1 className={styles.sectionHeadText}>Projects.</h1>
+      </motion.div>
+    </>
   )
 }
 
-export default Works
+export default SectionWrapper(Works, "works");
